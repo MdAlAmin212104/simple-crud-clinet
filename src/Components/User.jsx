b862d8202fe3f103ca932142e62098034ab9ff2b
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-04-21 23:07:18 +0600

    client site user delete method added

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -3,11 +3,30 @@ import { useLoaderData } from 'react-router-dom';
 
 const User = () => {
       const user = useLoaderData();
+      const handleDeleteUser = (id) => {
+            console.log(id);
+            fetch(`http://localhost:3000/users/${id}`, {
+                  method: 'DELETE',
+
+            })
+                  .then(res => res.json())
+                  .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                              alert('user deleted successfully')
+                        }
+                  })
+
+      }
       return (
             <div>
                   <h2>{user.length}</h2>
                   {
-                        user.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                        user.map(user => <p
+                              key={user._id}
+                        >{user.name} : {user.email} <button
+                                    onClick={() => handleDeleteUser(user._id)}
+                              >X</button></p>)
                   }
             </div>
       );
